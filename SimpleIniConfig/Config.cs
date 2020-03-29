30c48d1d8ba99132d905e96bf115b67549890e55
mrflashstudio <mrflashstudio@gmail.com>
2020-03-29 12:25:42 +0300

    Initial commit

---
+++ SimpleIniConfig/Config.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace SimpleIniConfig
+{
+    public class Config
+    {
+        public static readonly NumberFormatInfo NumberFormat = new CultureInfo(@"en-US", false).NumberFormat;
+
+        private string pathToConfig = @"config.ini";
+        private List<Setting> localConfig = new List<Setting>();
+
+        public Config(string pathToConfig = null)
+        {
+            if (!string.IsNullOrEmpty(pathToConfig))
+                this.pathToConfig = pathToConfig;
+
+            loadConfig();
+        }
+
+        ~Config() => saveConfig();
+
+        public T GetValue<T>(string settingName, T defaultValue)
+        {
+            var foundSetting = localConfig.Find(s => s.Name == settingName);
+            if (foundSetting != default)
+            {
+                var typeInfo = typeof(T).GetTypeInfo();
+                string typeName = typeInfo.Name.Replace("[]", string.Empty);
+                string enumAssemblyQualifiedName = typeInfo.AssemblyQualifiedName.Replace("[]", string.Empty);
+
+                if (Type.GetType(enumAssemblyQualifiedName).IsEnum)
+                {
+                    if (typeInfo.IsArray)
+                        return foundSetting.AsEnumArray<T>();
+
+                    return foundSetting.AsEnum<T>();
+                }
+
+                switch (typeName)
+                {
+                    case "String":
+                        if (typeInfo.IsArray)
+                            return (T)(object)foundSetting.AsStringArray();
+
+                        return (T)(object)foundSetting.AsString();
+                    case "Boolean":
+                        if (typeInfo.IsArray)
+                            return (T)(object)foundSetting.AsBooleanArray();
+
+                        return (T)(object)foundSetting.AsBoolean();
+                    case "Int32":
+                        if (typeInfo.IsArray)
+                            return (T)(object)foundSetting.AsIntArray();
+
+                        return (T)(object)foundSetting.AsInt();
+                    case "Single":
+                        if (typeInfo.IsArray)
+                            return (T)(object)foundSetting.AsFloatArray();
+
+                        return (T)(object)foundSetting.AsFloat();
+                    case "Double":
+                        if (typeInfo.IsArray)
+                            return (T)(object)foundSetting.AsDoubleArray();
+
+                        return (T)(object)foundSetting.AsDouble();
+                    default:
+                        return (T)(object)foundSetting.RawValue;
+                }
+            }
+            else
+                createNewEntry(settingName, defaultValue);
+
+            return defaultValue;
+        }
+
+        public void SetValue<T>(string settingName, T newValue)
+        {
+            var foundSetting = localConfig.Find(s => s.Name == settingName);
+            if (foundSetting != default)
+            {
+                foundSetting.RawValue = formatValue(newValue);
+                saveConfig();
+            }
+            else
+                createNewEntry(settingName, newValue);
+        }
+
+        private void createNewEntry<T>(string settingName, T value)
+        {
+            localConfig.Add(new Setting(settingName, formatValue(value)));
+            saveConfig();
+        }
+
+        private string formatValue<T>(T value)
+        {
+            var typeInfo = typeof(T).GetTypeInfo();
+            string typeName = typeInfo.Name.Replace("[]", string.Empty);
+
+            switch (typeName)
+            {
+                case "String":
+                    if (typeInfo.IsArray)
+                        return $"\"{string.Join("\",\"", (string[])(object)value)}\"";
+
+                    return $"\"{value}\"";
+                case "Single":
+                    if (typeInfo.IsArray)
+                        return string.Join(",", ((float[])(object)value).Select(f => f.ToString(NumberFormat)));
+
+                    return ((float)(object)value).ToString(NumberFormat);
+                case "Double":
+                    if (typeInfo.IsArray)
+                        return string.Join(",", ((double[])(object)value).Select(d => d.ToString(NumberFormat)));
+
+                    return ((double)(object)value).ToString(NumberFormat);
+                default:
+                    if (typeInfo.IsArray)
+                        return string.Join(",", ((IEnumerable)value).Cast<object>().Select(x => x.ToString()));
+
+                    return value.ToString();
+            }
+        }
+
+        private void ensureConfigFileExists()
+        {
+            if (!File.Exists(pathToConfig))
+                File.Create(pathToConfig).Close();
+        }
+
+        private void loadConfig()
+        {
+            ensureConfigFileExists();
+
+            string[] lines = File.ReadAllLines(pathToConfig);
+            foreach (string line in lines)
+                localConfig.Add(new Setting(line));
+        }
+
+        private void saveConfig()
+        {
+            ensureConfigFileExists();
+            File.WriteAllText(pathToConfig, string.Empty);
+
+            foreach (var setting in localConfig)
+                File.AppendAllText(pathToConfig, setting.ToString() + Environment.NewLine);
+        }
+    }
+}
---
+++ SimpleIniConfig/Setting.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SimpleIniConfig
+{
+    public class Setting
+    {
+        public string Name { get; private set; }
+
+        public string RawValue { get; set; }
+
+        public Setting(string name, string rawValue)
+        {
+            Name = name;
+            RawValue = rawValue;
+        }
+
+        public Setting(string rawLine)
+        {
+            string[] split = rawLine.Split('=');
+
+            Name = split.First().Trim();
+            RawValue = split.Last().Trim();
+        }
+
+        public T AsEnum<T>()
+        {
+            int intValue = 0;
+            if (int.TryParse(RawValue, out intValue))
+                return (T)(object)intValue;
+            else
+                return (T)Enum.Parse(typeof(T), RawValue, true);
+        }
+
+        public T AsEnumArray<T>()
+        {
+            string enumAssemblyQualifiedName = typeof(T).AssemblyQualifiedName.Replace("[]", string.Empty);
+            var enumType = Type.GetType(enumAssemblyQualifiedName);
+
+            string[] enumNames = ((IEnumerable)Enum.GetValues(enumType)).Cast<object>().Select(e => e.ToString()).ToArray();
+            int[] enumValues = (int[])Enum.GetValues(enumType);
+
+            return (T)(object)Array.ConvertAll(RawValue.Split(','), e => enumValues[Array.IndexOf(enumNames, e)]);
+        }
+
+        public string AsString()
+        {
+            return RawValue.ToString().Trim('"');
+        }
+
+        public string[] AsStringArray()
+        {
+            Match match = Regex.Match(RawValue, "[\\\"][^\\\"]*[\\\"][,]*");
+
+            List<string> elements = new List<string>();
+
+            while (match.Success)
+            {
+                string element = match.Value.TrimEnd(',');
+                elements.Add(element.Trim('"'));
+                match = match.NextMatch();
+            }
+
+            return elements.ToArray();
+        }
+
+        public bool AsBoolean()
+        {
+            return RawValue.ToLower() == "true" || RawValue == "1";
+        }
+
+        public bool[] AsBooleanArray()
+        {
+            return Array.ConvertAll(RawValue.Split(','), b => b.ToLower() == "true" || b == "1");
+        }
+
+        public int AsInt()
+        {
+            return int.Parse(RawValue);
+        }
+
+        public int[] AsIntArray()
+        {
+            return Array.ConvertAll(RawValue.Split(','), int.Parse);
+        }
+
+        public float AsFloat()
+        {
+            return float.Parse(RawValue, Config.NumberFormat);
+        }
+
+        public float[] AsFloatArray()
+        {
+            return Array.ConvertAll(RawValue.Split(','), f => float.Parse(f, Config.NumberFormat));
+        }
+
+        public double AsDouble()
+        {
+            return double.Parse(RawValue, Config.NumberFormat);
+        }
+
+        public double[] AsDoubleArray()
+        {
+            return Array.ConvertAll(RawValue.Split(','), f => double.Parse(f, Config.NumberFormat));
+        }
+
+        public override string ToString() => $"{Name} = {RawValue}";
+    }
+}
